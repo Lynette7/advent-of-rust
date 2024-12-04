8a2d4eac6ff9073f0237b5e322598d4dcc378cae
Lynette Mwangi <69351261+Lynette7@users.noreply.github.com>
2024-12-04 13:01:22 +0300

    chore: Kid struct and Impl

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,3 +1,38 @@
+pub const GOOD_WEIGHT: f32 = 1.0;
+pub const BAD_WEIGHT: f32 = 2.0;
+
+#[derive(Debug, PartialEq)]
+pub enum Niceness {
+    Nice(u32),
+    Naughty,
+}
+
+pub struct Kid {
+    pub name: String,
+    pub niceness: Niceness,
+}
+
+impl Kid {
+    pub fn is_nice(good_deeds: u32, bad_deeds: u32) -> bool {
+        if good_deeds == 0 && bad_deeds == 0 {
+            return false;
+        }
+
+        let ratio: f32 = good_deeds as f32 * GOOD_WEIGHT / ((good_deeds as f32 * GOOD_WEIGHT) + (bad_deeds as f32 * BAD_WEIGHT));
+        ratio >= 0.75
+    }
+
+    pub fn new(name: String, good_deeds: u32, bad_deeds: u32) -> Kid {
+        let niceness = if Self::is_nice(good_deeds, bad_deeds) {
+            Niceness::Nice(good_deeds)
+        } else {
+            Niceness::Naughty
+        };
+
+        Kid{ name, niceness }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
