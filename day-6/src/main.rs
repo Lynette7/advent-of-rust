029d0eace9a7be3f0165bdaba3508cf096bb1b64
Lynette Mwangi <69351261+Lynette7@users.noreply.github.com>
2024-12-06 23:06:03 +0300

    chore: longer wish function

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -1,3 +1,16 @@
+pub fn longer_wish<'a>(s1: &'a str, s2: &'a str) -> Option<&'a str> {
+    let string1 = s1.trim().chars().count();
+    let string2 = s2.trim().chars().count();
+
+    if string1 > string2 {
+        Some(s1)
+    } else if string2 > string1 {
+         Some(s2)
+    } else {
+        None
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
