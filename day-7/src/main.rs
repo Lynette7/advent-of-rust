ecb8b02a7a3d2b18ac323b28ae27b1b737598562
Lynette Mwangi <69351261+Lynette7@users.noreply.github.com>
2024-12-07 23:36:46 +0300

    chore: log system

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -1,3 +1,17 @@
+pub struct LogQuery<'a> {
+    logs: &'a Vec<String>,
+}
+
+impl<'a> LogQuery<'a> {
+    pub fn new(logs: &'a Vec<String>) -> Self {
+        LogQuery { logs }
+    }
+
+    pub fn search(&self, keyword: &str) -> Vec<String> {
+        self.logs.iter().filter(|log| log.contains(keyword)).collect()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
