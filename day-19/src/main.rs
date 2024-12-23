0d9863b1dc3900a517f3aa5b0890df4505ef9360
Lynette Mwangi <69351261+Lynette7@users.noreply.github.com>
2024-12-23 14:51:37 +0300

    chore: sleigh state

--- day-19/src/main.rs
+++ day-19/src/main.rs
@@ -1,3 +1,39 @@
+use std::marker::PhantomData;
+
+pub struct Empty;
+pub struct Ready;
+pub struct Flying;
+
+pub struct Sleigh<State> {
+    _state: PhantomData<State>,
+}
+
+impl Sleigh<Empty> {
+    pub fn new() -> Self {
+        Self { _state: PhantomData }
+    }
+
+    pub fn load(self) -> Sleigh<Ready> {
+        Sleigh { _state: PhantomData }
+    }
+}
+
+impl Sleigh<Ready> {
+    pub fn take_off(self) -> Sleigh<Flying> {
+        Sleigh { _state: PhantomData }
+    }
+
+    pub fn unload(self) -> Sleigh<Empty> {
+        Sleigh { _state: PhantomData }
+    }
+}
+
+impl Sleigh<Flying> {
+    pub fn land(self) -> Sleigh<Ready> {
+        Sleigh { _state: PhantomData }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
